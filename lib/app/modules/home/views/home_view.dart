acc67b91dfcef13c50539d95202421d753bd5fcd
Sangam Kumar Shrestha <2shrestha22@gmail.com>
2021-06-22 12:50:10 +0545

    vertical buttons

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -15,13 +15,38 @@ class HomeView extends GetView<HomeController> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
+          child: Stack(
             children: [
-              CustomeAppbar(),
-              SizedBox(
-                height: 5,
+              Positioned.fill(
+                left: 0,
+                child: Row(
+                  children: [
+                    RotatedBox(
+                      quarterTurns: -1,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          TextButton(onPressed: () {}, child: Text('Shop')),
+                          TextButton(onPressed: () {}, child: Text('Order')),
+                          TextButton(onPressed: () {}, child: Text('Bag')),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
               ),
-              SearchBar(),
+              Column(
+                children: [
+                  CustomeAppbar(),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  SearchBar(),
+                ],
+              ),
             ],
           ),
         ),
