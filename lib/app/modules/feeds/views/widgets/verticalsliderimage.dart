1eec2d4e655f47c77b7f6e96005dc6d13225e8f6
pranavo72bex <pranav072bex@ioepc.edu.np>
2021-06-24 09:31:02 +0545

    ui

--- lib/app/modules/feeds/views/widgets/verticalsliderimage.dart
+++ lib/app/modules/feeds/views/widgets/verticalsliderimage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class VerticalSliderImage extends StatelessWidget {
@@ -5,17 +7,48 @@ class VerticalSliderImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5),
-      width: 280,
-      height: 147,
-      decoration: BoxDecoration(
-        color: Colors.red,
-        image: DecorationImage(
-          image: AssetImage('assets/images/cardimage.png'),
-          fit: BoxFit.cover,
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.all(5),
+          width: 338,
+          height: 147,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            image: DecorationImage(
+              image: AssetImage('assets/images/cardimage.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          left: 0,
+          bottom: 0,
+          right: 0,
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 10.0,
+              sigmaY: 10.0,
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.zero,
+              child: Container(
+                height: 41,
+                width: 333,
+                color: Colors.black.withOpacity(0.8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Like", style: TextStyle(color: Colors.white)),
+                    Text("comment", style: TextStyle(color: Colors.white)),
+                    Text("share", style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
