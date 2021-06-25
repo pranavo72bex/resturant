02d3983d791dbb8d48b83b53c57aa3c41b538277
pranavo72bex <pranav072bex@ioepc.edu.np>
2021-06-25 21:42:08 +0545

    ui

--- lib/app/modules/merchant/view/widgets/customtabbar_mer.dart
+++ lib/app/modules/merchant/view/widgets/customtabbar_mer.dart
@@ -74,13 +74,13 @@ class CustomTabbarMerch extends StatelessWidget {
         children: [
           HorizontalCardMerch(),
           SearchbarMerchant(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Menu",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Text(
+          //     "Menu",
+          //     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
           MerchantverticalCard()
         ],
       ),
--- lib/app/modules/merchant/view/widgets/verticalcard_merchat.dart
+++ lib/app/modules/merchant/view/widgets/verticalcard_merchat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurantsapp/app/data/themedata/conts.dart';
 
 class MerchantverticalCard extends StatelessWidget {
   const MerchantverticalCard({Key? key}) : super(key: key);
@@ -6,9 +7,71 @@ class MerchantverticalCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(border: Border.all(width: 0.3)),
+      padding: EdgeInsets.all(8),
       height: 220,
       width: double.infinity,
-      color: Colors.red,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset("assets/icons/cross.png"),
+                Text(
+                  "Red velvet cake[Double Layer]",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text("in waffle cake"),
+                Text("₹499.99"),
+                customtextcontainer(),
+                Text(
+                  "long description about the product",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                )
+              ],
+            ),
+          ),
+          Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                child: Image.asset("assets/images/birimage.png"),
+              ),
+              Positioned(
+                left: 32,
+                bottom: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  height: 40,
+                  width: 80,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget customtextcontainer() {
+    return Container(
+      padding: EdgeInsets.all(3),
+      decoration: BoxDecoration(
+        border: Border.all(color: korange),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Text(
+        "Bestseller",
+        style: TextStyle(color: korange),
+      ),
     );
   }
 }
