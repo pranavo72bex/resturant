0494c2e0c5c26b82dd957eabdbb79be49bfa2573
pranavo72bex <pranav072bex@ioepc.edu.np>
2021-07-06 10:16:31 +0545

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ Future<void> main() async {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      // initialBinding: AllControllerBinding(),
       title: "Application",
       getPages: AppPages.routes,
       theme: Apptheme().defaultheme,
